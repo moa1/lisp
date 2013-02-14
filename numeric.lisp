3c4f77902f2aba1c35fdc09fa880f11843734c93
toni <toni@purasuchikku.mydomain>
2013-02-14 20:53:45 +0100

    added to numeric.lisp num>, num<, num>=, num<=, num=, ifelse

--- numeric.lisp
+++ numeric.lisp
@@ -102,3 +102,62 @@
   (loop for x in a collect
        (loop for y in b collect
 	    (funcall f x y))))
+
+(defun num>-2 (a b)
+  (num-iterate-2 a b #'>))
+
+(defun num<-2 (a b)
+  (num-iterate-2 a b #'<))
+
+(defun num>=-2 (a b)
+  (num-iterate-2 a b #'>=))
+
+(defun num=-2 (a b)
+  (num-iterate-2 a b #'=))
+
+(defun num<=-2 (a b)
+  (num-iterate-2 a b #'<=))
+
+(defun num> (&rest r)
+  (reduce #'num>-2 r))
+
+(defun num< (&rest r)
+  (reduce #'num<-2 r))
+
+(defun num>= (&rest r)
+  (reduce #'num>=-2 r))
+
+(defun num<= (&rest r)
+  (reduce #'num<=-2 r))
+
+(defun num= (&rest r)
+  (reduce #'num=-2 r))
+
+(defun ifelse (test then else)
+  "Returns a value with the same shape as test from then or else depending on
+whether test is T or NIL. then and else must have the same shape, and this shape
+must be smaller than test."
+  (cond
+    ((num-is-number test) (if test then else))
+    ((num-is-vector test)
+     (cond ((num-is-number then) (loop for a in test collect (if a then else)))
+	   ((num-is-vector then)
+	    (loop for a in test for b in then for c in else collect
+		 (if a b c)))
+	   ((num-is-matrix then) (error "then and else must have a shape smaller than test"))
+	   (t (error "unknown num then"))))
+    ((num-is-matrix test)
+     (cond 
+       ((num-is-number then) (loop for row in test collect
+				  (loop for x in row collect (if x then else))))
+       ((num-is-vector then) (loop for row in test collect
+				  (loop for x in row
+				     for y in then for z in else collect
+				       (if x y z))))
+       ((num-is-matrix then) (loop for rowa in test
+				for rowb in then for rowc in else collect
+				  (loop for x in rowa
+				     for y in rowb for z in rowc collect
+				       (if x y z))))
+       (t (error "unknown num then"))))
+    (t (error "unknown num test"))))
