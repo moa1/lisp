8f3642bf2ea8fc35c5874d5df6bc2d311f363f96
toni <toni@purasuchikku.mydomain>
2013-10-17 02:20:19 +0200

    add to joy-arch-plot.R using a log file from the command line.

--- joy-arch-plot.R
+++ joy-arch-plot.R
@@ -1,5 +1,13 @@
-t <- read.table("/tmp/log.txt")
-#t <- read.table("/tmp/log2.txt")
+# specifying a non-default filename: cat joy-arch-plot.R | R --no-save --args /tmp/log.txt
+filename <- "/tmp/log.txt"
+co <- commandArgs()
+args <- which(co=="--args")+1
+if (length(args)>0) {
+  filename <- co[args]
+}
+cat("plotting filename=",filename,"\n")
+
+t <- read.table(filename)
 # 3 columns for fitness parameters
 fits <- 3
 # mut_length_const is the number of parameters after the fitness parameters and before the joy-ops frequencies (see *mut-length-const*)
@@ -20,9 +28,9 @@ if (all(t$V2 <= 0)) {
 plot(t$V3, type="b", log="y")
 
 # plot mutate parameters
-matplot(t[,seq(fits+1,fits+mut_length_const*2,2)], type="b", cex=0.5)
-matplot(t[,seq(fits+2,fits+mut_length_const*2,2)], type="b", cex=0.5)
+matplot(t[,seq(fits+1,fits+mut_length_const*2,2)], type="b", cex=0.25)
+matplot(t[,seq(fits+2,fits+mut_length_const*2,2)], type="b", cex=0.25)
 # plot joy-ops frequencies
-matplot(t[,seq(fits+mut_length_const*2+1,length(t),2)], type="b", cex=0.5)
-matplot(t[,seq(fits+mut_length_const*2+2,length(t),2)], type="b", cex=0.5)
+matplot(t[,seq(fits+mut_length_const*2+1,length(t),2)], type="b", cex=0.25)
+matplot(t[,seq(fits+mut_length_const*2+2,length(t),2)], type="b", cex=0.25)
 dev.off()
