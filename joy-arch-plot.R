f39adcee70d80e7e5a718eb50895c46ecd4a32b7
toni <toni@purasuchikku.mydomain>
2013-11-06 01:46:37 +0100

    add to joy-arch-plot.R writing plot titles.

--- joy-arch-plot.R
+++ joy-arch-plot.R
@@ -16,21 +16,21 @@ mut_length_const <- 6+3
 postscript("/tmp/log.ps")
 par(mfrow=c(3,2))
 if (all(t$V2 <= 0)) {
-  plot(-t$V2, type="b", log="y", ylim=range(c(-t$V1,-t$V2)))
+  plot(-t$V2, type="b", log="y", ylim=range(c(-t$V1,-t$V2)), main="fitness")
   lines(-t$V1, type="b")
 } else if (all(t$V2 >= 0)) {
-  plot(t$V2, type="b", log="y", ylim=range(c(t$V1,t$V2)))
+  plot(t$V2, type="b", log="y", ylim=range(c(t$V1,t$V2)),main="fitness")
   lines(t$V1, type="b")
 } else {
-  plot(t$V2, type="b")
+  plot(t$V2, type="b",main="fitness")
   lines(t$V1, type="b")
 }
-plot(t$V3, type="b", log="y")
+plot(t$V3, type="b", log="y",main="fitness stddev")
 
 # plot mutate parameters
-matplot(t[,seq(fits+1,fits+mut_length_const*2,2)], type="b", cex=0.25)
-matplot(t[,seq(fits+2,fits+mut_length_const*2,2)], type="b", cex=0.25)
+matplot(t[,seq(fits+1,fits+mut_length_const*2,2)], type="b", cex=0.25,main="mutate parameters")
+matplot(t[,seq(fits+2,fits+mut_length_const*2,2)], type="b", cex=0.25,main="mutate parameters stddev")
 # plot joy-ops frequencies
-matplot(t[,seq(fits+mut_length_const*2+1,length(t),2)], type="b", cex=0.25)
-matplot(t[,seq(fits+mut_length_const*2+2,length(t),2)], type="b", cex=0.25)
+matplot(t[,seq(fits+mut_length_const*2+1,length(t),2)], type="b", cex=0.25,main="joy-ops freqs")
+matplot(t[,seq(fits+mut_length_const*2+2,length(t),2)], type="b", cex=0.25,main="joy-ops freqs stddev")
 dev.off()
