3828dbb7b3f3f9bdc9c460aed617005bc601d9ac
toni <toni@purasuchikku>
2014-12-16 00:31:08 +0100

    add shuffle.lisp, which implements shuffle*, an in-place shuffler.

---
+++ shuffle.lisp
@@ -0,0 +1,35 @@
+(defmethod shuffle* ((l list))
+  (labels ((rec (rest n)
+	     (when (> n 0)
+	       (let* ((r (nthcdr (random n) rest))
+		      (a (car rest)))
+		 (setf (car rest) (car r))
+		 (setf (car r) a))
+	       (rec (cdr rest) (1- n)))))
+    (rec l (length l)))
+  l)
+
+(defmethod shuffle* ((v simple-vector))
+  (let ((n (array-dimension v 0)))
+    (labels ((rec (i)
+	       (when (> i 0)
+		 (let ((r (random i))
+		       (a (aref v i)))
+		   (setf (aref v i) (aref v r))
+		   (setf (aref v r) a))
+		 (rec (1- i)))))
+      (rec (1- n))))
+  v)
+
+;;(let ((a (make-array 10 :initial-contents (loop for i below 10 collect i))))
+;;  (utils:timecps (lambda () nil)))
+
+(defun time-shuffle* ()
+  (loop for i below 20 do
+       (let* ((n (ceiling (expt 1.5 i)))
+	      (l (loop for i below n collect i))
+	      (a (make-array n :initial-contents l)))
+	 (format t "~A ~G ~G~%"
+		 n
+		 (nth-value 1 (utils:timesec (lambda () (shuffle* l))))
+		 (nth-value 1 (utils:timesec (lambda () (shuffle* a))))))))
