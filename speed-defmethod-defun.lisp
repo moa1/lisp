b1ae23b9e78583390b546fd2f988cc0e225d9694
toni <toni@pc1400>
2015-03-11 18:19:29 +0100

    add speed test to determine if there is a difference in speed between calling a generic function with many defined methods or few.

--- speed-defmethod-defun.lisp
+++ speed-defmethod-defun.lisp
@@ -1,5 +1,5 @@
 (load "~/quicklisp/setup.lisp")
-(ql:quickload 'utils)
+(ql:quickload :utils)
 
 (defun test-1 (a)
   a)
@@ -17,6 +17,44 @@
   a)
 
 (defun time-them ()
-  (timediff (test-1 5) (test-2 5) :showtimes t)
-  (timediff (test-1 5) (test-3 5) :showtimes t)
-  (timediff (test-2 5) (test-3 5) :showtimes t))
+  (utils:timediff (test-1 5) (test-2 5) :showtimes t)
+  (utils:timediff (test-1 5) (test-3 5) :showtimes t)
+  (utils:timediff (test-2 5) (test-3 5) :showtimes t))
+
+;; test whether calling a generic with many defined methods takes longer than one with few.
+
+(defmethod test-many ((a number))
+  a)
+
+(defmethod test-many ((a symbol))
+  a)
+
+(defmethod test-many ((a list))
+  a)
+
+(defmethod test-many ((a cons))
+  a)
+
+(defmethod test-many ((a standard-object))
+  a)
+
+(defmethod test-few ((a number))
+  a)
+
+(defun test-many-few-methods ()
+  (utils:timediff (test-many 5) (test-few 5) :showtimes t))
+
+;; with many types
+
+(loop for i below 100 do
+     (let ((name (intern (format nil "STRUCT-~A" i))))
+       (eval `(progn
+		(defstruct ,name a b c)
+		(defmethod test-verymany ((a ,name)) a)))))
+
+(defmethod test-veryfew ((a struct-0))
+  a)
+
+(defun test-verymany-veryfew-methods ()
+  (let ((s (make-struct-0 :a 1 :b 2 :c 3)))
+    (utils:timediff (test-verymany s) (test-veryfew s) :showtimes t)))
