b50c0700398cbbc1398c153c41e8f25988eb843a
toni <toni@pc1400>
2015-06-14 18:50:55 +0200

    add function software-render-pixels, which draws single pixels onto the screen, but slowly. start the not-yet-working function software-render-texture, which shall write onto a pixels array directly.

--- sdl2-example.lisp
+++ sdl2-example.lisp
@@ -1,8 +1,11 @@
 ;; taken from basic.lisp from cl-sdl2, but removed the opengl requirement.
 
-(in-package :sdl2-examples)
+(load "/home/toni/quicklisp/setup.lisp")
+(ql:quickload :sdl2)
 
-(require :sdl2)
+;;(in-package :sdl2-examples)
+;;(require :sdl2)
+
 
 (defun basic-test ()
   "The kitchen sink."
@@ -18,7 +21,7 @@
 	    (haptic ()))
 
 	;; basic window/gl setup
-	(format t "Setting up window/gl.~%")
+	(format t "Setting up window: ~A.~%" win)
 	(finish-output)
 
 	(format t "Opening game controllers.~%")
@@ -88,3 +91,126 @@
 	   do (progn
 		(sdl2:game-controller-close controller)
 		(sdl2:haptic-close (cdr (assoc i haptic)))))))))
+
+(defun software-render-pixels ()
+  "Software renderer example, drawing single pixels on the screen."
+  (sdl2:with-init (:everything)
+    (format t "Using SDL Library Version: ~D.~D.~D~%"
+            sdl2-ffi:+sdl-major-version+
+            sdl2-ffi:+sdl-minor-version+
+            sdl2-ffi:+sdl-patchlevel+)
+    (finish-output)
+
+    (sdl2:with-window (win :flags '(:shown))
+      ;; basic window/gl setup
+      (format t "Setting up window: ~A.~%" win)
+      (finish-output)
+
+      ;;(let ((wrend (sdl2:create-renderer win -1 '(:software :targettexture))))
+      (let ((wrend (sdl2:create-renderer win -1 '(:software))))
+
+	(format t "Window renderer: ~A~%" wrend)
+	(finish-output)
+
+	(sdl2-ffi.functions:sdl-set-render-draw-color wrend 255 0 0 255)
+
+	;; main loop
+	(format t "Beginning main loop.~%")
+	(finish-output)
+	(sdl2:with-event-loop (:method :poll)
+	  (:keyup
+	   (:keysym keysym)
+	   (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
+	     (sdl2:push-event :quit)))
+
+	  (:mousemotion
+	   (:x x :y y :xrel xrel :yrel yrel :state state)
+	   (format t "Mouse motion abs(rel): ~a (~a), ~a (~a)~%Mouse state: ~a~%"
+		   x xrel y yrel state))
+
+	  (:idle
+	   ()
+	   ;;(sdl2-ffi.functions:sdl-set-render-draw-color wrend (random 256) (random 256) (random 256) (random 256))
+	   ;;(sdl2-ffi.functions:sdl-render-draw-point wrend (random 800) (random 600))
+	   (sdl2-ffi.functions:sdl-render-draw-point wrend (random 800) (random 600))
+	   (sdl2:render-present wrend)
+	   )
+
+	  (:quit () t))
+
+	(format t "End of main loop.~%")
+	(finish-output)
+	))))
+
+;; (defun software-render-texture ()
+;;   "Software renderer example, drawing a texture on the screen.
+;; See SDL-wiki/MigrationGuide.html#If_your_game_just_wants_to_get_fully-rendered_frames_to_the_screen.
+;; "
+;;   (sdl2:with-init (:everything)
+;;     (format t "Using SDL Library Version: ~D.~D.~D~%"
+;;             sdl2-ffi:+sdl-major-version+
+;;             sdl2-ffi:+sdl-minor-version+
+;;             sdl2-ffi:+sdl-patchlevel+)
+;;     (finish-output)
+
+;;     (sdl2:with-window (win :flags '(:shown))
+;;       ;; basic window/gl setup
+;;       (format t "Setting up window: ~A.~%" win)
+;;       (finish-output)
+
+;;       (let* ((wrend (sdl2:create-renderer win -1 '(:software :targettexture)))
+;; 	     (tex (sdl2:create-texture wrend :ARGB8888 :streaming 800 600))
+;; 	     (sur (sdl2:create-rgb-surface 800 600 32
+;; 					   :r-mask #x00ff0000
+;; 					   :g-mask #x0000ff00
+;; 					   :b-mask #x000000ff
+;; 					   :a-mask #xff000000))
+;; 	     ;; see .../cl-autowrap-20141217-git/cl-plus-c.md: "We may access the various fields as follows:"
+;; 	     (pix (plus-c:c-ref sur SDL2-FFI:SDL-SURFACE :pixels)))
+
+;; ;; get sur->pixels using (cffi:foreign-slot-value sur ...).
+
+;; 	(format t "Window renderer: ~A~%" wrend)
+;; 	(format t "Texture: ~A~%" tex)
+;; 	(format t "Surface: ~A~%" sur)
+;; 	(format t "Pixels: ~A~%" pix)
+;; 	(finish-output)
+
+;; 	(sdl2-ffi.functions:sdl-set-render-draw-color wrend 255 0 0 255)
+
+;; 	;; main loop
+;; 	(format t "Beginning main loop.~%")
+;; 	(finish-output)
+;; 	(sdl2:with-event-loop (:method :poll)
+;; 	  (:keyup
+;; 	   (:keysym keysym)
+;; 	   (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
+;; 	     (sdl2:push-event :quit)))
+
+;; 	  (:mousemotion
+;; 	   (:x x :y y :xrel xrel :yrel yrel :state state)
+;; 	   (format t "Mouse motion abs(rel): ~a (~a), ~a (~a)~%Mouse state: ~a~%"
+;; 		   x xrel y yrel state))
+
+;; 	  (:idle
+;; 	   ()
+;; 	   (setf (cffi:mem-aref pix :uint32 (+ (random 800) (* (random 600) 800))) (random (expt 2 32)))
+;; 	   (sdl2:update-texture tex pix)
+;; 	   ;;TODO: call SDL_RenderClear(sdlRenderer);
+;; 	   (sdl2:render-copy wrend tex)
+;; 	   (sdl2:render-present wrend)
+;; 	   )
+
+;; 	  (:quit () t))
+
+;; 	(format t "End of main loop.~%")
+;; 	(finish-output)
+;; 	))))
+
+;; (let* ((sur (sdl2:create-rgb-surface 800 600 32
+;; 				     :r-mask #x00ff0000
+;; 				     :g-mask #x0000ff00
+;; 				     :b-mask #x000000ff
+;; 				     :a-mask #xff000000))
+;;        (pix (plus-c:c-ref sur SDL2-FFI:SDL-SURFACE :pixels)))
+;;   (cffi:mem-aref pix :UINT32 1))
