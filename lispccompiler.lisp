a4b30887011300782188ed5f36546bf3110435e8
toni <toni@pc1400>
2016-11-14 23:34:16 +0100

    cut out lisp forms parsing code and put it into its own package WALKER-PLUS.

--- lispccompiler.lisp
+++ lispccompiler.lisp
@@ -1,7 +1,7 @@
 ;;;; Lisp to C Compiler
 
 (load "~/quicklisp/setup.lisp")
-(ql:quickload :walker)
+(ql:quickload '(:walker :walker-plus))
 
 ;;;; Package
 
@@ -53,52 +53,9 @@
 
 ;;;; The backend stuff implementing the special operators of Lisp and some macros.
 
-(defclass multiple-value-bind-form (walker:form walker:body-form)
-  ((vars :initarg :vars :accessor walker:form-vars :type list :documentation "list of VARs")
-   (values :initarg :values :accessor walker:form-values :type generalform)
-   (declspecs :initarg :declspecs :accessor walker:form-declspecs :type list)))
-(defclass values-form (walker:form walker:body-form)
-  ())
-(defclass nth-value-form (walker:form) ;TODO: implement NTH-VALUE-form, but I think this will not be easy since there is no way in LISP to specify multiple value types for a form. Probably implementing this will need an automatic type inferencer, like NIMBLE.
-  ((value :initarg :value :accessor walker:form-value :type generalform)
-   (values :initarg :values :accessor walker:form-values :type generalform)))
-(defclass defun-form (walker:fun-binding)
-  ())
-(defclass declaim-form (walker:form)
-  ((declspecs :initarg :declspecs :accessor walker:form-declspecs :type list)))
-(defclass funcall-form (walker:form)
-  ((var :initarg :sym :accessor walker:form-var :type walker:var)
-   (arguments :initarg :arguments :accessor walker:form-arguments :type list :documentation "list of GENERALFORMs")))
 (defclass prind-form (walker:form walker:body-form)
   ((lexicalnamespace :initarg :lexicalnamespace :accessor walker:form-lexicalnamespace)
    (freenamespace :initarg :freenamespace :accessor walker:form-freenamespace)))
-
-(defmethod print-object ((object multiple-value-bind-form) stream)
-  (print-unreadable-object (object stream :type t :identity t)
-    (format stream "~S ~S ~A" (walker:form-vars object) (walker:form-values object) (walker:format-body object t nil))))
-(defmethod print-object ((object values-form) stream)
-  (print-unreadable-object (object stream :type t :identity t)
-    (format stream "~A" (walker:format-body object nil nil))))
-(defmethod print-object ((object nth-value-form) stream)
-  (print-unreadable-object (object stream :type t :identity t)
-    (format stream "~A ~A" (walker:form-value object) (walker:form-values object))))
-(defmethod print-object ((object defun-form) stream)
-  (print-unreadable-object (object stream :type t :identity t)
-    (format stream "~S ~S ~A" (walker:form-sym object) (walker:form-llist object) (walker:format-body object t t))))
-(defmethod print-object ((object declaim-form) stream)
-  (print-unreadable-object (object stream :type t :identity t)
-    (format stream "~A" (walker:form-declspecs object))))
-(defmethod print-object ((object funcall-form) stream)
-  (print-unreadable-object (object stream :type t :identity t)
-    (format stream "~S" (walker:form-var object))
-    (loop for arg in (walker:form-arguments object) do
-	 (format stream " ~S" arg))))
-
-(defun parse-some-macros-p (form lexical-namespace free-namespace parent &key customparsep-function customparse-function customparsedeclspecp-function customparsedeclspec-function)
-  (declare (ignore customparsep-function customparse-function customparsedeclspecp-function customparsedeclspec-function lexical-namespace free-namespace parent))
-  (and (listp form)
-       (let ((head (car form)))
-	 (find head '(multiple-value-bind values nth-value defun declaim funcall)))))
 
 (defun parse-some-macros (form lexical-namespace free-namespace parent &key customparsep-function customparse-function customparsedeclspecp-function customparsedeclspec-function)
   (declare (optimize (debug 3)))
@@ -114,132 +71,24 @@
     (let ((head (car form))
 	  (rest (cdr form)))
       (cond
-	((eq head 'multiple-value-bind)
-	 (assert (and (consp rest) (listp (car rest)) (listp (cadr rest))) () "Cannot parse MULTIPLE-VALUE-BIND-form ~S" form)
-	 (let* ((vars-form (let ((vars-form (car rest))) (loop for var in vars-form do (assert (symbolp var) () "VARs in MULTIPLE-VALUE-BIND-form must be symbols, not ~S" var)) vars-form))
-		(values-form (cadr rest))
-		(body (cddr rest))
-		(current (make-instance 'multiple-value-bind-form :parent parent))
-		(parsed-vars (loop for var-form in vars-form collect (reparse var-form current)))
-		(parsed-values (reparse values-form current)))
-	   (multiple-value-bind (body parsed-declspecs)
-	       (walker:parse-declaration-in-body body lexical-namespace free-namespace current :customparsedeclspecp-function customparsedeclspecp-function :customparsedeclspec-function customparsedeclspec-function)
-	     (setf (walker:form-vars current) parsed-vars)
-	     (setf (walker:form-values current) parsed-values)
-	     (setf (walker:form-declspecs current) parsed-declspecs)
-	     (setf (walker:form-body current) (parse-body body current)))
-	   current))
-	((eq head 'values)
-	 (let* ((objects-form rest)
-		(current (make-instance 'values-form :parent parent))
-		(parsed-objects (loop for object-form in objects-form collect (reparse object-form current))))
-	   (setf (walker:form-body current) parsed-objects)
-	   current))
-	((eq head 'nth-value)
-	 (assert (and (consp rest) (consp (cdr rest)) (null (cddr rest))) () "Cannot parse NTH-VALUE-form ~S" form)
-	 (let* ((value-form (car rest))
-		(values-form (cadr rest))
-		(current (make-instance 'nth-value-form :parent parent))
-		(parsed-value (reparse value-form current))
-		(parsed-values (reparse values-form current)))
-	   (setf (walker:form-value current) parsed-value)
-	   (setf (walker:form-values current) parsed-values)
-	   current))
-	((eq head 'defun)
-	 (multiple-value-bind (fun-type name) (walker:valid-function-name-p (car rest))
-	   (let* ((lambda-list-and-body (cdr rest))
-		  (block-name (ecase fun-type ((walker:fun) name) ((walker:setf-fun) (cadr name)))) ;CLHS Glossary "function block name" defines "If the function name is a list whose car is setf and whose cadr is a symbol, its function block name is the symbol that is the cadr of the function name."
-		  (blo (make-instance 'walker:blo :name block-name :freep nil :jumpers nil))
-		  (current (make-instance 'defun-form :parent parent :blo blo))
-		  (sym (walker:namespace-lookup/create 'walker:fun name lexical-namespace free-namespace)))
-	     (walker:parse-and-set-functiondef lambda-list-and-body #'walker:parse-ordinary-lambda-list (walker:augment-lexical-namespace blo lexical-namespace) free-namespace current :customparsep-function customparsep-function :customparse-function customparse-function :customparsedeclspecp-function customparsedeclspecp-function :customparsedeclspec-function customparsedeclspec-function)
-	     (setf (walker:nso-definition blo) current)
-	     (setf (walker:form-sym current) sym)
-	     current)))
-	((eq head 'declaim)
-	 (let* ((declspecs rest)
-		(current (make-instance 'declaim-form :parent parent))
-		(parsed-declspecs (walker:parse-declspecs declspecs lexical-namespace free-namespace current :customparsedeclspecp-function customparsedeclspecp-function :customparsedeclspec-function customparsedeclspec-function)))
-	   (setf (walker:form-declspecs current) parsed-declspecs)
-	   current))
-	((eq head 'funcall)
-	 (assert (symbolp (car rest)) () "Cannot parse FUNCALL-form ~S" form)
-	 (let* ((fun-sym (car rest))
-		(arg-forms (cdr rest))
-		(sym (walker:namespace-lookup/create 'walker:var fun-sym lexical-namespace free-namespace))
-		(current (make-instance 'funcall-form :parent parent :sym sym))
-		(parsed-arguments nil))
-	   (loop do
-		(when (null arg-forms) (return))
-		(assert (and (consp arg-forms) (listp (cdr arg-forms))) () "Invalid argument rest ~S in function or macro application" arg-forms)
-		(push (reparse (car arg-forms) current) parsed-arguments)
-		(setf arg-forms (cdr arg-forms)))
-	   (setf (walker:form-arguments current) (nreverse parsed-arguments))
-	   current))
 	((eq head 'prind)
 	 (let* ((current (make-instance 'prind-form :parent parent :lexicalnamespace lexical-namespace :freenamespace free-namespace)))
 	   (setf (walker:form-body current) (parse-body rest current))
 	   current))
 	))))
 
-(defun deparse-multiple-value-bind-form (ast parent recurse-function)
-  (declare (ignore parent))
-  (list* 'multiple-value-bind
-	 (mapcar (lambda (var) (funcall recurse-function var ast)) (walker:form-vars ast))
-	 (funcall recurse-function (walker:form-values ast) ast)
-	 (walker:deparse-body ast recurse-function t nil)))
-(defun deparse-values-form (ast parent recurse-function)
-  (declare (ignore parent))
-  (list* 'values (walker:deparse-body ast recurse-function nil nil)))
-(defun deparse-nth-value-form (ast parent recurse-function)
-  (declare (ignore parent))
-  (list* 'nth-value
-	 (funcall recurse-function (walker:form-value ast) ast)
-	 (funcall recurse-function (walker:form-values ast) ast)))
-(defun deparse-defun-form (ast parent recurse-function)
-  (declare (ignore parent))
-  (list* 'defun
-	 (funcall recurse-function (walker:form-sym ast) ast)
-	 (walker:deparse-body ast recurse-function t t)))
-(defun deparse-declaim-form (ast parent recurse-function)
-  (declare (ignore parent))
-  (list* 'declaim
-	 (funcall recurse-function (walker:form-declspecs ast) ast)))
-(defun deparse-funcall-form (ast parent recurse-function)
-  (declare (ignore parent))
-  (list* 'funcall
-	 (funcall recurse-function (walker:form-var ast) ast)
-	 (mapcar (lambda (arg) (funcall recurse-function arg ast)) (walker:form-arguments ast))))
 (defun deparse-prind-form (ast parent recurse-function)
   (declare (ignore parent))
   (list* 'prind
 	 (walker:deparse-body ast recurse-function nil nil)))
 
-(defun deparse-typecase (ast parent &key &allow-other-keys)
-  (declare (ignore parent))
-  (typecase ast
-    (multiple-value-bind-form #'deparse-multiple-value-bind-form)
-    (values-form #'deparse-values-form)
-    (nth-value-form #'deparse-nth-value-form)
-    (defun-form #'deparse-defun-form)
-    (declaim-form #'deparse-declaim-form)
-    (funcall-form #'deparse-funcall-form)
-    (prind-form #'deparse-prind-form)
-    (t nil)))
-
-(defun deparse-some-macros (ast parent)
-  (declare (optimize (debug 3)))
-  (if (deparse-typecase ast parent)
-      (funcall (deparse-typecase ast parent) ast parent #'deparse-some-macros)
-      (walker:deparse ast parent :customdeparsep-function #'deparse-typecase :customdeparse-function #'deparse-some-macros)))
-
 (defun walker-parse (form parent &key lexical-namespace free-namespace)
   (let ((lexical-namespace (if lexical-namespace lexical-namespace (walker:make-empty-lexical-namespace)))
 	(free-namespace (if free-namespace free-namespace (walker:make-default-free-common-lisp-namespace))))
-    (walker:parse form lexical-namespace free-namespace parent :customparsep-function #'parse-some-macros-p :customparse-function #'parse-some-macros)))
+    (walker:parse form lexical-namespace free-namespace parent :customparsep-function #'walker-plus:parse-p :customparse-function #'walker-plus:parse)))
 
 (defun walker-deparse (ast parent)
-  (deparse-some-macros ast parent))
+  (walker-plus:deparse ast parent))
 
 ;; Types
 
@@ -476,6 +325,7 @@ Return the augmented NAMESPACE."
     (t (error "unknown LISP type ~S" type))))
     
 (defun augment-namespace-with-builtin-function (symbol c-name argument-types values-types namespace)
+  (declare (optimize (debug 3)))
   (let* ((ast (walker:parse-with-empty-namespaces `(flet ((,symbol ()))
 						     (declare (ftype (function (,@argument-types) (values ,@values-types)) ,symbol))
 						     (,symbol))))
@@ -787,9 +637,9 @@ Return the augmented NAMESPACE."
 (defun emitc-function-application-funcall (ast namespace values)
   (declare (optimize (debug 3)))
   (prind (walker-deparse ast nil))
-  (let* ((fun (etypecase ast (walker:application-form (walker:form-fun ast)) (funcall-form (walker:form-var ast))))
+  (let* ((fun (etypecase ast (walker:application-form (walker:form-fun ast)) (walker-plus:funcall-form (walker:form-var ast))))
 	 (arguments (walker:form-arguments ast))
-	 (c-fun (etypecase ast (walker:application-form (find-fun-for-lisp-fun namespace fun arguments)) (funcall-form (find-var-for-lisp-var namespace fun))))
+	 (c-fun (etypecase ast (walker:application-form (find-fun-for-lisp-fun namespace fun arguments)) (walker-plus:funcall-form (find-var-for-lisp-var namespace fun))))
 	 (fun-type (nso-type c-fun))
 	 (fun-arguments-type (cadr fun-type))
 	 (fun-values-type (caddr fun-type))
@@ -823,7 +673,7 @@ Return the augmented NAMESPACE."
 				    (c-declaration psym (format nil "&~A" (nso-name sym))))))
 		     arguments-code
 		     (format nil "~A(~A);"
-			     (etypecase ast (walker:application-form (nso-name c-fun)) (funcall-form (format nil "(*~A)" (nso-name c-fun))))
+			     (etypecase ast (walker:application-form (nso-name c-fun)) (walker-plus:funcall-form (format nil "(*~A)" (nso-name c-fun))))
 			     (join-strings
 			      (concatenate 'list
 					   (mapcar #'nso-name (mapcar #'car arguments-syms))
@@ -839,7 +689,7 @@ Return the augmented NAMESPACE."
 	 (c-fun (find-fun-for-lisp-fun namespace fun lisp-arguments)))
     (c-code (c-assign (car values) (nso-name c-fun)))))
 
-(defmethod emitc ((ast funcall-form) (namespace namespace) values)
+(defmethod emitc ((ast walker-plus:funcall-form) (namespace namespace) values)
   (emitc-function-application-funcall ast namespace values))
   
 (defmethod emitc ((ast walker:if-form) (namespace namespace) values)
@@ -860,7 +710,7 @@ Return the augmented NAMESPACE."
 		       (c-code (format nil "else")
 			       (c-scope else-code)))))))
 
-(defmethod emitc ((ast values-form) (namespace namespace) values)  
+(defmethod emitc ((ast walker-plus:values-form) (namespace namespace) values)  
   (declare (optimize (debug 3)))
   (let* ((body (walker:form-body ast)))
     (assert (<= (length body) (length values)))
@@ -868,7 +718,7 @@ Return the augmented NAMESPACE."
 	     (loop for object in body for value in values collect
 		  (emitc object namespace (list value)))))))
 
-(defmethod emitc ((ast multiple-value-bind-form) (namespace namespace) values)  
+(defmethod emitc ((ast walker-plus:multiple-value-bind-form) (namespace namespace) values)  
   (declare (optimize (debug 3)))
   (let* ((vars-namespace namespace) ;the namespace augmented with all binding variables
 	 (vars-syms (loop for walker-sym in (walker:form-vars ast) collect
@@ -900,7 +750,7 @@ Return the augmented NAMESPACE."
 		 (c-scope (c-declaration pvar-sym (format nil "&~A" (nso-name c-var)))
 			  (emitc form-value namespace (list pvar-sym)))))))
 
-(defmethod emitc ((ast defun-form) (namespace namespace) values)
+(defmethod emitc ((ast walker-plus:defun-form) (namespace namespace) values)
   (let* ((defun-namespace namespace)
 	 (walker-sym (walker:form-sym ast))
 	 (declspec-type0 (sym-declspec-type walker-sym))
@@ -917,7 +767,7 @@ Return the augmented NAMESPACE."
 (defmethod emitc ((ast walker:progn-form) (namespace namespace) values)
   (c-code (c-scope (emitc-body (walker:form-body ast) namespace values))))
 
-(defmethod emitc ((ast declaim-form) (namespace namespace) values)  
+(defmethod emitc ((ast walker-plus:declaim-form) (namespace namespace) values)  
   nil)
 
 (defmethod emitc ((ast walker:tagbody-form) (namespace namespace) values)
