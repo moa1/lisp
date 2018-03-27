c301a17f6da93b146f3db310aa6a14638cd5afb5
toni <toni@zweihorn>
2018-03-27 15:54:50 +0200

    transfer a comment how to declare function types from nti-subtypep.lisp to test-declare.lisp.

--- test-declare.lisp
+++ test-declare.lisp
@@ -23,3 +23,32 @@
   (let ((a 1))
     (declare (foo 1)) ;produces a warning both in SBCL and CLISP.
     a))
+
+;;;; This tests how type information of functions can be represented in lisp.
+
+;; for example types could be represented as a list of OR-concatenated types, like in
+(declaim (ftype (or (function ((eql t)) integer) (function (null) single-float)) declare-ftype))
+(defun declare-ftype (a)
+  (if a 1 1.0))
+
+;; The following has the disadvantage that the declared type is more general than intended, and the correspondence of input types and output types is not clearly readable, is: (DECLAIM (FTYPE (FUNCTION ((OR FLOAT INTEGER) (OR FLOAT INTEGER)) (OR (VECTOR INTEGER) (VECTOR FLOAT))) F1))
+(declaim (ftype (function ((or (eql t) null)) (or integer single-float)) declare-ftype-too-general))
+(defun declare-ftype-too-general (a)
+  (if a 1 1.0))
+
+#|
+;; declaring function type as concatenated declare specifications doesn't work, since they are interpreted as AND-concatenated types, leading to conflicts.
+(defun test2 ()
+  (flet ((f1 (a b)
+	   (make-array 2 :initial-contents (list a b))))
+    (declare (ftype (function (integer integer) (vector integer)) f1)
+	     (ftype (function (float float) (vector float)) f1))
+
+    (f1 2 3)
+    (f1 2.0 3.0)))
+|#
+
+#|
+(defun f1 (a b)
+  (make-array 2 :initial-contents (list a b)))
+|#
