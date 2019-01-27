f7680a8bd5c759ca0fb3c691acaa962f813eda4d
toni <toni@zweihorn>
2019-01-27 23:36:29 +0100

    add comment to nsm.lisp about how to nest state machines. I don't know it yet.

--- nsm.lisp
+++ nsm.lisp
@@ -108,3 +108,35 @@
 (defun sm-loops (sm)
   "Return a list of circles of states that state-machine SM goes through when starting at all possible states."
   )
+
+#|
+Possible nestings:
+e.g. SM(4), i.e. with 4 states
+NSM(1, SM1(4), SM2(4), SM3(4), SM4(4)), i.e. of level 1, and with 4 state machines, each of which have 4 possible states.
+
+How to do input-output of the state-machine and a nested state-machine? I could make it all stateless, ... or with state? ... hm... In the end I want to be able to make the (nested?) state-machine be able to compute the operation "-" (minus).
+
+One way would be to mark an output state by an infinite circle of 1 state, e.g. 0->5, 5->5. This way the operation "-" (minus) could be modeled as:
+output states:
+0->0, 1->1, 2->2, 3->3
+input states, where the numbers in brackets are the implicit input states (numbers; think of X(A,B)->C as C==A-B):
+3(0,0)->0,  4(0,1)->3,  5(0,2)->2,  6(0,3)->1
+7(1,0)->1,  8(1,1)->0,  9(1,2)->3,  10(1,3)->2
+11(2,0)->2, 11(2,1)->1, 12(2,2)->0, 13(2,3)->3
+14(3,0)->3, 15(3,1)->2, 16(3,2)->1, 17(3,3)->0
+This is equivalent to
+(DEFUN MINUS-SM (A B)
+  (ASSERT (AND (<= 0 A 3) (<= 0 B 3)))
+  (LOGAND (- A B) 3)).
+
+Another way would be to divide a state into output,input,memory bits. With memory bits a state-machine could model a number storage that has 1 input bit, and output bits=memory bits, and if the input bit is set to 0, then the output bits are stored in the memory bits, and for the input bit set to 1 the memory bits are stored in in the output bits.
+e.g. a state machine MEM-SM(3 bits output,4 bits input,3 bits memory) like:
+(LET ((N 0))
+  (DEFUN MEM (I M)
+    (ASSERT (AND (<= 0 I 1) (<= 0 M 7)))
+    (WHEN (= I 0) (SETF N M))
+    N)).
+
+Nesting of state machines could be done with:
+NSM(1, SM1(4), SM2(4), SM3(4), SM4(4)), i.e. of level 1, and with 4 state machines, each of which output 4 possible states.
+|#
